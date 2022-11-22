25e22ba974158f2a965c33bca2f02254497965ca
devcoderoad <devdef20@gmail.com>
2022-11-22 18:52:42 +0700

    chore: initiate changes

--- pages/about.js
+++ pages/about.js
@@ -5,9 +5,9 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
 /* Components */
-import ProTip from "@src/ProTip";
-import Link from "@src/Link";
-import Copyright from "@src/Copyright";
+import ProTip from "@src/componentsProTip";
+import Link from "@src/components/Link";
+import Copyright from "@src/components/Copyright";
 
 export default function About() {
   return (
